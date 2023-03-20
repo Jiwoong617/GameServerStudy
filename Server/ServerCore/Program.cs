04a731ff7c6a345d8591d2f00f1c5e0da49dbebb
허지웅 <joe0617160@naver.com>
2023-03-20 21:19:17 +0900

    간단한 소켓 프로그래밍

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,39 +1,59 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using System.Threading;
 
 namespace ServerCore
 {
     class Program
     {
-        volatile static bool _stop =false;
-
-        static void ThreadMain()
-        {
-            Console.WriteLine("Start");
-
-            while (!_stop)
-            {
-
-            }
-
-            Console.WriteLine("End");
-        }
-
         static void Main(string[] args)
         {
-            Task t = new Task(ThreadMain);
-            t.Start();
+            //DNS
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-            Thread.Sleep(1000);
+            //문지기
+            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
-            _stop = true;
+            try
+            {
+                //문지기 교육
+                listenSocket.Bind(endPoint);
 
-            Console.WriteLine("STOP 호출");
-            Console.WriteLine("종료 대기중");
+                //영업 시작
+                //backlog : 최대 대기수
+                listenSocket.Listen(10);
 
-            t.Wait();
+                while (true)
+                {
+                    Console.WriteLine("Listening...");
 
-            Console.WriteLine("종료 성공");
+                    //손님 입장
+                    Socket clientSocket = listenSocket.Accept();
+
+                    //받기
+                    byte[] recvBuff = new byte[1024];
+                    int recvBytes = clientSocket.Receive(recvBuff);
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                    Console.WriteLine($"[From Client] {recvData}");
+
+                    //보내기
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to Server!");
+                    clientSocket.Send(sendBuff);
+
+                    //쫒아내기
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
