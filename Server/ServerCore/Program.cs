5ba0eee6f78a10d7000c9a3ee5714356234224b0
허지웅 <joe0617160@naver.com>
2023-03-22 18:52:48 +0900

    Receive Send 비동기방식

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -14,21 +14,17 @@ namespace ServerCore
         {
             try
             {
-                //받기
-                byte[] recvBuff = new byte[1024];
-                int recvBytes = clientSocket.Receive(recvBuff);
-                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
-                Console.WriteLine($"[From Client] {recvData}");
+                Session session = new Session();
+                session.Start(clientSocket);
 
-                //보내기
                 byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to Server!");
-                clientSocket.Send(sendBuff);
+                session.Send(sendBuff);
 
-                //쫒아내기
-                clientSocket.Shutdown(SocketShutdown.Both);
-                clientSocket.Close();
+                Thread.Sleep(1000);
+
+                session.Disconnect();
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
             }
